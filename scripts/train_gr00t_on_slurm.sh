51006292784a1081fe39f6755da268bbc645c395
crislmfroes <cris.lima.froes@gmail.com>
2026-04-30 15:20:52 -0300

    Update SLURM script

--- scripts/train_gr00t_on_slurm.sh
+++ scripts/train_gr00t_on_slurm.sh
@@ -6,11 +6,16 @@
 #SBATCH --partition=v100
 #SBATCH --mail-user=cris.lima.froes@gmail.com
 #SBATCH --mail-type=ALL
-git clone https://github.com/crislmfroes/experiment.git
-git clone https://github.com/NVIDIA/Isaac-GR00T.git
-git clone https://huggingface.co/datasets/crislmfroes/xarm6-sim-pose-cond-v4-pose-rand_v2.0
-curl -LsSf https://astral.sh/uv/install.sh | sh
-uv sync --python 3.10 --project Isaac-GR00T
-uv run python -c "import gr00t; print('GR00T installed successfully')" --project Isaac-GR00T >> results/out1.txt
+cd /home/users/crislmfroes/Isaac-GR00T
+uv run python \
+    gr00t/experiment/launch_finetune.py \
+    --base-model-path nvidia/GR00T-N1.7-3B \
+    --dataset-path /home/users/crislmfroes/xarm6-sim-pose-cond-v4-pose-rand_v2.0 \
+    --modality-config-path /home/users/crislmfroes/experiment/source/fbot_arena/fbot_arena/policies/gr00t/xarm6_config.py --embodiment-tag NEW_EMBODIMENT \
+    --num-gpus 1 \
+    --output-dir /home/users/crislmfroes/results/gr00t_n17_fbot_arena_pose_rand \
+    --max-steps 2000 \
+    --global-batch-size 1 \
+    --dataloader-num-workers 0 --gradient-accumulation-steps 64 --save-total-limit 1
 #sleep 1m
 #nvidia-smi >> results/out1.txt
