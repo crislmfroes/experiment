9d4a41d9fdbdb3b85e2ae9eff091c462e5bc808b
crislmfroes <cris.lima.froes@gmail.com>
2026-04-30 15:35:53 -0300

    Update SLURM script

--- scripts/train_gr00t_on_slurm.sh
+++ scripts/train_gr00t_on_slurm.sh
@@ -6,8 +6,13 @@
 #SBATCH --partition=v100
 #SBATCH --mail-user=cris.lima.froes@gmail.com
 #SBATCH --mail-type=ALL
-cd ./Isaac-GR00T
-/home/users/crislmfroes/.local/bin/uv run python \
+#module load uv
+git clone https://github.com/NVIDIA/Isaac-GR00T.git
+git clone https://huggingface.co/datasets/crislmfroes/xarm6-sim-pose-cond-v4-pose-rand_v2.0
+git clone https://github.com/crislmfroes/experiment.git
+cd Isaac-GR00T
+uv sync --python 3.10
+uv run python \
     gr00t/experiment/launch_finetune.py \
     --base-model-path nvidia/GR00T-N1.7-3B \
     --dataset-path /home/users/crislmfroes/xarm6-sim-pose-cond-v4-pose-rand_v2.0 \
